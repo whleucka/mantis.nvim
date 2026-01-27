cfd08098f56a47f932bb7078fe6a4052a3556905
William Hleucka <william.hleucka@gmail.com>
2026-01-27 07:51:48 -0700

    feat: MantisIssue command

--- lua/mantis/commands.lua
+++ lua/mantis/commands.lua
@@ -9,3 +9,20 @@ vim.api.nvim_create_user_command('MantisSelectHost', function()
 end, {
   desc = 'Select Mantis Host',
 })
+
+vim.api.nvim_create_user_command('MantisIssue', function(opts)
+  local issue_id = tonumber(opts.args)
+  if not issue_id then
+    vim.notify("Invalid issue ID: " .. opts.args, vim.log.levels.ERROR)
+    return
+  end
+  local state = require("mantis.state")
+  if not state.api then
+    vim.notify("No host selected. Run :MantisSelectHost first.", vim.log.levels.WARN)
+    return
+  end
+  require("mantis.ui").view_issue(issue_id)
+end, {
+  nargs = 1,
+  desc = 'View Mantis Issue by ID',
+})
